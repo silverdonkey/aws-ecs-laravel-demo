e4c02ebe5fd886487d598fd7fbe33970530f8392
Nikolay Orozov <dev@niko-consulting.de>
2023-02-09 16:10:29 +0100

    wip: use just one default config

--- config/database.php
+++ config/database.php
@@ -138,21 +138,13 @@ return [
         'clusters' => [
             'default' => [
                 [ # cluster suppose multiple hosts in it, so wrapping each host in cluster into array make sense
+                    'scheme'   => env('REDIS_SCHEME', 'tcp'),
                     'url' => env('REDIS_URL'),
                     'host' => env('REDIS_HOST', '127.0.0.1'),
                     'password' => env('REDIS_PASSWORD', null),
                     'port' => env('REDIS_PORT', '6379'),
                     'database' => env('REDIS_DB', '0'),
-                ],
-            ],
-
-            'cache' => [
-                [
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'password' => env('REDIS_PASSWORD', null),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'database' => env('REDIS_CACHE_DB', '1'),
+                    'timeout' => 15,
                 ],
             ],
 
